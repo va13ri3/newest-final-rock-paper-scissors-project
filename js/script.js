749a3d28f484342b71e13f74da4d333a647e6820
Valerie Gonzalez <127VGonzalezMorales@frhsd.com>
2026-05-19 13:48:52 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,91 @@
+let playerScore = 0;
+let computerScore = 0;
+let ties = 0; // optional
+
+const choices = ["Rock", "Paper", "Scissors"];
+
+document.querySelector("#rock").addEventListener("click", function () {
+    playRound("Rock");
+});
+
+document.querySelector("#paper").addEventListener("click", function () {
+    playRound("Paper");
+});
+
+document.querySelector("#scissors").addEventListener("click", function () {
+    playRound("Scissors");
+});
+
+document.querySelector("#reset").addEventListener("click", resetGame);
+
+
+function playRound(playerChoice) {
+    console.log("Playing Round " + playerChoice);
+    const computerChoice = getComputerChoice();
+    const result = determineWinner(playerChoice, computerChoice);
+
+    updateScore(result);
+    displayResult(playerChoice, computerChoice, result);
+}
+
+function getComputerChoice() {
+    const index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
+
+function determineWinner(player, computer) {
+
+    if (player === computer) {
+        return "tie";
+    }
+
+    if (
+        (player === "Rock" && computer === "Scissors") ||
+        (player === "Paper" && computer === "Rock") ||
+        (player === "Scissors" && computer === "Paper")
+    ) {
+        return "player";
+    }
+
+    return "computer";
+}
+
+function updateScore(result) {
+    if (result === "player") {
+        playerScore++;
+    } else if (result === "computer") {
+        computerScore++;
+    } else {
+        ties++;
+    }
+   
+}
+
+function displayResult(playerChoice, computerChoice, result) {
+    let resultText = " ";    
+    if (result === "player") {
+        resultText = "You win!";
+    }
+    else if (result === "computer") {
+        resultText = "Computer wins!";
+    }
+    else {
+        resultText = "It's a tie!";
+    }
+        document.querySelector("#outcome").textContent = playerChoice + " vs " + computerChoice;
+        document.querySelector("#result").textContent = resultText;
+        document.querySelector("#playerScore").textContent = playerScore;
+        document.querySelector("#computerScore").textContent = computerScore;
+}
+
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    ties = 0;
+   
+    document.querySelector("#playerScore").textContent = playerScore;
+    document.querySelector("#computerScore").textContent = computerScore;
+    document.querySelector("#outcome").textContent = '';
+    document.querySelector("#result").textContent = " ";
+}
